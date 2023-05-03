1344186290d0c24a142e7185b4f3b460bb6e1645
Gudjos <116514537+Gudjos@users.noreply.github.com>
2023-05-03 18:54:22 +0100

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@ function rectangularCollision({rectangle1, rectangle2}) {
         rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
         rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
         rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-        player.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
     )
 }
 function determineWinner({player, enemy, timerID}) {
